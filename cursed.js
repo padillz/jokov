06f77ffbf9b8405af1c608de6d175546823a082f
dilzx <padillz575@gmail.com>
2025-12-20 18:14:27 +0700

    Update token list

--- cursed.js
+++ cursed.js
@@ -4,6 +4,6 @@
     "8001616412:AAE40A1EYv7OXIuVuP7MXgqrvvT0V0-VkTg",
     "8593657674:AAGRwyXBgSL2JVoE4ogzizbdmEMAI01nwv8",
     "8487908057:AAG9RZs8JIWfaKoW7nElSLaaEZqg0UqUCms",
-    "/addtoken"
+    "8078635526:AAFo3MqdiBw_ynpyqe8N1QTRQxVF4v9DmUU"
   ]
 }
